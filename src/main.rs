4e3e64de5b4a2cf715d87106084d4309d2377c69
Junfan Zhang <zuston@apache.org>
2023-05-27 20:46:45 +0800

    Set max message size to max

--- src/main.rs
+++ src/main.rs
@@ -135,8 +135,9 @@ async fn main() -> Result<()> {
     info!("Starting GRpc server with port:[{}] ......", rpc_port);
     let shuffle_server = DefaultShuffleServer::from(app_manager_ref);
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), rpc_port as u16);
+    let service = ShuffleServerServer::new(shuffle_server).max_decoding_message_size(usize::MAX).max_encoding_message_size(usize::MAX);
     Server::builder()
-        .add_service(ShuffleServerServer::new(shuffle_server))
+        .add_service(service)
         .serve(addr)
         .await?;
 
