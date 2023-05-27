01483b270d6fd3448ac596881029ea233bc60f7a
Junfan Zhang <zuston@apache.org>
2023-05-27 20:09:49 +0800

    Support set datanode ip

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,15 @@
 use std::net::IpAddr;
 
 pub fn get_local_ip() -> Result<IpAddr, std::io::Error> {
-    let socket = std::net::UdpSocket::bind("0.0.0.0:0")?;
-    socket.connect("8.8.8.8:80")?;
-    let local_addr = socket.local_addr()?;
-    Ok(local_addr.ip())
+    let ip = std::env::var("DATANODE_IP");
+    if ip.is_ok() {
+        Ok(ip.unwrap().parse().unwrap())
+    } else {
+        let socket = std::net::UdpSocket::bind("0.0.0.0:0")?;
+        socket.connect("8.8.8.8:80")?;
+        let local_addr = socket.local_addr()?;
+        Ok(local_addr.ip())
+    }
 }
 
 #[cfg(test)]
