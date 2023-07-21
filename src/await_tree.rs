60a150dcd0b4edcf7dd0b1e4b04d3bff4529d50f
Junfan Zhang <zuston@apache.org>
2023-07-21 22:28:48 +0800

    chore: remove printing out await-tree into log (#42)

--- src/await_tree.rs
+++ src/await_tree.rs
@@ -6,22 +6,5 @@ use std::time::Duration;
 use log::info;
 
 lazy_static! {
-    pub static ref AWAIT_TREE_REGISTRY: Arc<Mutex<Registry<u64>>> = {
-        let registry = Arc::new(Mutex::new(Registry::new(await_tree::Config::default())));
-        schedule_print_out_await_tree(registry.clone());
-        registry
-    };
-}
-
-fn schedule_print_out_await_tree(registry: Arc<Mutex<Registry<u64>>>) {
-    tokio::spawn(async move {
-        loop {
-            tokio::time::sleep(Duration::from_secs(60)).await;
-            let temp_registry = registry.lock().await;
-            info!("========================== await tree ==========================");
-            for (_, tree) in temp_registry.iter() {
-                info!("{tree}");
-            }
-        }
-    });
+    pub static ref AWAIT_TREE_REGISTRY: Arc<Mutex<Registry<u64>>> = Arc::new(Mutex::new(Registry::new(await_tree::Config::default())));
 }
